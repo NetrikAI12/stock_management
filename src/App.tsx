a5ec7ab4e96797e7e66f98c4e916cc6bca73a63a
Aravind2634 <akaravind110@gmail.com>
2025-09-05 12:55:41 +0530

    feat: your descriptive message here

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// test commit for Git push
 import React, { useState, useEffect } from 'react';
 import { AuthProvider, useAuth } from './contexts/AuthContext';
 import { StockProvider, useStock } from './contexts/StockContext';
